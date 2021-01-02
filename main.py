69c615c6fb0dc897561fd4c602d60801f79f7145
Jason Zhang <zjjc123@hotmail.com>
2021-01-02 00:17:59 -0800

    Converting to Numpy + Re-rg + Quantile Plotter @christine8888

--- main.py
+++ main.py
@@ -1,23 +1,71 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from importer import import_dream_data
 from simulations.barter import simulate_barter
 
-blaze_drop_chance = 0.5
-ender_eye_drop_chance = 0.05
-
-num_runs = 1000000
+# Constants
+ender_pearl_drop_chance = 0.05
+num_runs = 1000
 ingots = 262
 
+# Data
 dream_barter_data = import_dream_data("./data/dream_pearls.csv")
-barter_simulation = simulate_barter(num_runs, ingots, ender_eye_drop_chance, best_run = True)
+barter_simulation = simulate_barter(num_runs, ingots, ender_pearl_drop_chance)
 
-print("Data Collected!")
+# Plots
+def plot_runs():
+    for run in barter_simulation:  
+        plt.plot(run, 'b', lw = 0.5)
+    plt.plot(dream_barter_data, 'r', lw = 0.7)
+    plt.xlabel('Cumulative Ingots Bartered')
+    plt.ylabel('Ender Pearls')
+    plt.show()
 
-for run in barter_simulation:  
-    plt.plot(run, 'b', lw = 0.5)
+def plot_best_run():
+    best_run = barter_simulation[0]
 
-plt.plot(dream_barter_data, 'r', lw = 0.7)
-plt.xlabel('Cumulative Ingots Bartered')
-plt.ylabel('Ender Pearls')
-plt.show()
+    for run in barter_simulation:  
+        if run[-1] > best_run[-1]:
+            best_run = run
+
+    plt.plot(best_run, 'b', lw = 0.5)
+    plt.plot(dream_barter_data, 'r', lw = 0.7)
+    plt.xlabel('Cumulative Ingots Bartered')
+    plt.ylabel('Ender Pearls')
+    plt.show()
+
+def get_quantiles(runs, low, high):
+    lows = np.zeros(runs.shape[1])
+    highs = np.zeros(runs.shape[1])
+    for i in range(runs.shape[1]):
+        lows[i] = np.quantile(runs[:,i],low)
+        highs[i] = np.quantile(runs[:,i],high)
+    return lows, highs
+
+def plot_quantiles():
+    # 1 sigma bounds
+    low_1sigma, high_1sigma = get_quantiles(barter_simulation,0.15865,0.84135)
+    plt.fill_between(np.arange(ingots),low_1sigma,high_1sigma,color='b',alpha=0.5)
+
+    # 2 sigma bounds
+    low_2sigma, high_2sigma = get_quantiles(barter_simulation,0.02275,0.97725)
+    plt.fill_between(np.arange(ingots),low_2sigma,high_2sigma,color='b',alpha=0.3)
+
+    # 3 sigma bounds
+    low_3sigma, high_3sigma = get_quantiles(barter_simulation,0.00135,0.99865)
+    plt.fill_between(np.arange(ingots),low_3sigma,high_3sigma,color='b',alpha=0.1)
+
+    plt.plot(dream_barter_data, 'r', lw = 0.7)
+    plt.xlabel('Cumulative Ingots Bartered')
+    plt.ylabel('Ender Pearls')
+    plt.show()
+
+# Plotting all runs and dream's run
+plot_runs()
+
+# Plotting the best of the runs and dream's run
+plot_best_run()
+
+# Plotting the quantiles and dream's run
+plot_quantiles()
--- simulations/barter.py
+++ simulations/barter.py
@@ -1,22 +1,16 @@
 import random
+import numpy as np
 
-def simulate_barter(num_runs, ingots, ender_eye_drop_chance, best_run = False):
-    runs = [[]]
-    best_total = 0
+def simulate_barter(num_runs, ingots, ender_eye_drop_chance):
+    runs = np.zeros((num_runs,ingots))
     for x in range(num_runs):
-        run = [0] * ingots
         for y in range(ingots):
             success = 0
             if (random.random() < ender_eye_drop_chance):
                 success = 1
             if (y >= 1):
-                run[y] = run[y - 1] + success
+                runs[x][y] = runs[x][y-1] + success
             else:
-                run[y] = success
-        if (best_run):
-            if (run[-1] > best_total): 
-                runs[0] = run
-                best_total = run[-1]
-        else:
-            runs.append(run)
+                runs[x][y] = success
     return runs
+    
